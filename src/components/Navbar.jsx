137ed9fcdb7d09340984987861d72c5313210c25
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-23 13:00:29 +0100

    starting

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav__container'>
+        <div className="nav">
+            <ul>
+                <li>Home </li>
+                <li>History</li>
+                <li>Amission</li>
+                <li>About us</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+                <li>Login Dashboard</li>
+                <li>se</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
