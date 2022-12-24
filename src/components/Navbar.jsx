ec517409293a93ae6dbf39973da1d1acd3d21706
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-24 01:56:39 +0100

    finished basic responsive nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import {BsSearch} from 'react-icons/bs'
 
 
 const Navbar = () => {
   const [width, setWidth] = useState(window.innerWidth);
+  const [showSidebar, setShowSidebar] = useState(false);
   const checkResize = () => {
     setWidth(window.innerWidth)
   }
@@ -14,26 +16,31 @@ const Navbar = () => {
       window.removeEventListener('resize', checkResize);
     }
       
-  })
+  });
+
+  const handleClick = () => {
+    setShowSidebar(prev => !prev)
+    console.log(showSidebar);
+  }
   return (
     <div className='nav__container'>
         <div className="block-header ">
           <div className='bg-r head'>
-            <img src="logo" alt="" />
+            {/* <img src="logo" alt="" /> */}
             <h1>ANSAR-UD-DEEN ACADEMY</h1>
+          {width < 760 && <div className="hamburger" onClick={handleClick}></div>}
           </div>
-          {width < 760 && <div className="hamburger"></div>}
         </div>
-        <div className="nav bg-r">
-            <ul className='nav__content'>
+        <div className={`${showSidebar ? 'shw' : 'noshw'} nav bg-r`}>
+            <ul className= ' nav__content'>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/history">History</NavLink>
-                <NavLink to = "/admission">Amission</NavLink>
+                <NavLink to = "/admission">Admission</NavLink>
                 <NavLink to = "/about_us">About us</NavLink>
                 <NavLink to = "/gallery">Gallery</NavLink>
                 <NavLink to = "/contact">Contact</NavLink>
                 <NavLink to = "/login">Login Dashboard</NavLink>
-                <img src="" alt="search" />
+                <BsSearch />
             </ul>
         </div>
         
