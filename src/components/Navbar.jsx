afb527fd84af5a55f84b054d681cb06053085162
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-24 01:02:24 +0100

    kinda responsive nav + add tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 const Navbar = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const checkResize = () => {
+    setWidth(window.innerWidth)
+  }
+  useEffect(()=> {
+    window.addEventListener('resize', checkResize)
+    console.log(width);
+    return () => {
+      window.removeEventListener('resize', checkResize);
+    }
+      
+  })
   return (
     <div className='nav__container'>
-        <div className="block-header">
+        <div className="block-header ">
+          <div className='bg-r head'>
             <img src="logo" alt="" />
             <h1>ANSAR-UD-DEEN ACADEMY</h1>
+          </div>
+          {width < 760 && <div className="hamburger"></div>}
         </div>
-        <div className="nav">
-            <ul>
+        <div className="nav bg-r">
+            <ul className='nav__content'>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/history">History</NavLink>
                 <NavLink to = "/admission">Amission</NavLink>
@@ -20,6 +36,7 @@ const Navbar = () => {
                 <img src="" alt="search" />
             </ul>
         </div>
+        
     </div>
   )
 }
