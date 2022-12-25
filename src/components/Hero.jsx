20e8506385fa830c758f5790b71139420461ebf2
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-25 22:59:15 +0100

    style button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-gradient-to-r from-purple-500 to-pink-500 min-h-[50vh]'>
-        <div className='bg-r'>
+    <div className='bg-gradient-to-r from-purple-500 to-pink-500 min-h-[50vh] '>
+        <div className='bg-r grid '>
 
         <h1 className='text-3xl font-bold text-center py-[2rem]'>Empowering Student knowledge</h1>
         <p>Lorem, ipsum dolor sit amet consectetur 
@@ -14,7 +14,7 @@ const Hero = () => {
               Et exercitationem hic reprehenderit totam,
                error numquam iste aut tenetur perspiciatis
             eveniet harum fugiat voluptates sit suscipit amet fuga dolor!</p>
-            <button className='btn p-[.7rem] rounded-md my-[1rem]'>Learn More</button>
+            <button className='btn p-[.7rem] rounded-md my-[1rem] mr-[0]'>Learn More</button>
         </div>
     </div>
   )
